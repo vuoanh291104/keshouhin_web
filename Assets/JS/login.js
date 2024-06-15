b8fc931ad7c784cdb6bb573ce73f2b7d97349449
オアイン <134583482+vuoanh291104@users.noreply.github.com>
2024-06-15 21:31:14 +0700

    up nốt

--- Assets/JS/login.js
+++ Assets/JS/login.js
@@ -8,10 +8,36 @@ document.addEventListener('DOMContentLoaded',(event)=>{
     
     const offSignIn = () => signIn.style.display = 'none'
 
-    btnOK.addEventListener('click',()=>{
-        alert('Success');
-        offSignIn()
-    })
+    // btnOK.addEventListener('click',()=>{
+    //     alert('Success');
+    //     offSignIn()
+    // })
+    document.getElementById('btnOK').addEventListener('click', function() {
+        var userNameInput = document.getElementById('txtUserName');
+        var passwordInput = document.getElementById('txtPassword');
+    
+        var userName = userNameInput.value.trim();
+        var password = passwordInput.value.trim();
+    
+        // Reset the input borders
+        userNameInput.classList.remove('error');
+        passwordInput.classList.remove('error');
+    
+        if (userName && password) {
+            
+            alert('Thông báo: Thành công!');
+        } else {
+            
+            if (!userName) {
+                userNameInput.classList.add('error');
+            }
+            if (!password) {
+                passwordInput.classList.add('error');
+            }
+            alert('Vui lòng nhập đầy đủ thông tin.');
+        }
+    });
+    
     btnCancel.addEventListener('click',()=>{
         // Xu ly su kien click button Cancel
         offSignIn()
