103f30ebedc1e60ba0bee98a442b69cd19851b40
Nguyễn Hữu Niên <huunien734@gmail.com>
2024-06-18 22:10:52 +0700

    ~blabla_btn

--- Assets/JS/item.js
+++ Assets/JS/item.js
@@ -1,18 +1,29 @@
 $(document).ready(function () {
-    btnTru.addEventListener('click', function () {
-        changeValue(-1);
-    });
-    btnCong.addEventListener('click', function () {
-        changeValue(1);
-    });
+    let btnTru = [], btnCong = [], va = [];
+
+    btnCong.push(document.getElementById('cong1'));
+    btnCong.push(document.getElementById('cong2'));
+    btnCong.push(document.getElementById('cong3'));
+    btnTru.push(document.getElementById('tru1'));
+    btnTru.push(document.getElementById('tru2'));
+    btnTru.push(document.getElementById('tru3'));
+    va.push(document.getElementById('value1'));
+    va.push(document.getElementById('value2'));
+    va.push(document.getElementById('value3'));
+
+    for (let i = 0; i < 3; i++) {
+        btnCong[i].addEventListener('click', function () {
+            changeValue(1, va[i]);
+        });
+        btnTru[i].addEventListener('click', function () {
+            changeValue(-1, va[i]);
+        });
+    }
 });
 
-function changeValue(a) {
-    var btnTru = document.getElementById('tru');
-    var btnCong = document.getElementById('cong');
-    var va = document.getElementById('value');
-    if (btnCong == null || btnTru == null || va == null) {
-        console.log("have null");
+function changeValue(a, va) {
+    if (va == null) {
+        console.log("value is null");
         return;
     }
     const x = Number(va.value) + Number(a);
